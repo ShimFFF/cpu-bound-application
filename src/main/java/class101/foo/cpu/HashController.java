dcc20ef3b3b98428f598b3f17e760224389fd678
ShimFFF <shimseawon1510@gmail.com>
2024-01-31 16:21:02 +0900

    :sparkles: Feat: 컨트롤러 수정

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello chapter 3";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
